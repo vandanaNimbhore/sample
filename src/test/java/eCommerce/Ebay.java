0dfad0cf161c2341234372b941905f722cc323c5
bmsce1234 <rahuls.me17@bmsce.ac.in>
2023-08-01 15:36:52 +0530

    (bmsce1234) added new ebay task

---
+++ src/test/java/eCommerce/Ebay.java
@@ -0,0 +1,23 @@
+package eCommerce;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Ebay {
+  @Test
+  public void ebay() throws InterruptedException 
+  {
+	  WebDriver driver=  new ChromeDriver();
+	  Reporter.log("opening ebay", false);
+	 // Thread.sleep(3000);
+	  driver.get("https://www.ebay.com/");
+	  Thread.sleep(7000);
+	  driver.close(); 
+	  
+	  
+	  
+	  
+  }
+}
